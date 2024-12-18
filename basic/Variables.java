0e4e30309c4ba23ed2bc29f61ef5c6bd6dec9e91
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-18 23:59:39 +0530

    added required vars

--- basic/Variables.java
+++ basic/Variables.java
@@ -1,7 +1,11 @@
 package basic;
 
 public class Variables {
+    int one=2;      //Instance Variable
+    static  int one1=3;     //Static variable
     public static void main(String[] args) {
-
+        int one=1;   //local variable
+        System.out.println(one+" "+one1);
     }
+
 }
