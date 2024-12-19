87a9a1c62ea30410004c842ff358318b894c8fd5
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-19 14:46:48 +0530

    two operators program added

---
+++ basic/OperatorsArith.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class OperatorsArith {
+    public static void main(String[] args) {
+        System.out.println("12+24/29*2="+12+24/29*2);
+    }
+}
---
+++ basic/OperatorsRel.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class OperatorsRel {
+    public static void main(String[] args) {
+        if(2>22){
+            System.out.println(2>22);
+        } else if (2==22) {
+            System.out.println("2==22");
+        } else if (2!=22) {
+            System.out.println("2!=22");
+        }
+    }
+}
