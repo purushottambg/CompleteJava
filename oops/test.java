0d2f3eed2d0bf642d1fe44bbc6aa20f51240e9b4
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-19 23:59:31 +0530

    first OOPS

--- oops/test.java
+++ oops/test.java
@@ -1,4 +1,15 @@
 package oops;
 
 public class test {
+    public static void main(String[] args) {
+        ObjectToBeCreated objectToBeCreated = new ObjectToBeCreated(22);
+
+    }
+}
+
+class ObjectToBeCreated{
+    private  int one;
+    ObjectToBeCreated(int varVal){
+        this.one=varVal;
+    }
 }
