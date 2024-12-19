c2061709c91e7fd62df0ebe9e6d774553d741c75
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-19 23:57:02 +0530

    Starting with OOPs Code

---
+++ basic/InstanceOfOperator.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class InstanceOfOperator {
+    public static void main(String[] args) {
+        if("Pallavi" instanceof String){  //instanceof is an operator which returns the boolean value
+            System.out.println("Yes, it's String value");
+        }
+    }
+}
