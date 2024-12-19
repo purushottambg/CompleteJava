9d8256cc3e28846968c51f4252ff10de98961a0a
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-19 11:06:59 +0530

    User defined or non primitive datatype

---
+++ basic/NonPrimitive.java
@@ -0,0 +1,9 @@
+package basic;
+import basic.BankDataType.*;
+public class NonPrimitive {
+    public static void main(String[] args) {
+        BankDataType bankDataType = new
+                BankDataType(243562534, "Pallavi Purushottam Gutthe","Kalyani nagar pune", 7385465.45d, "Purushottam Bharat Gutthe");
+        bankDataType.showDetails();
+    }
+}
