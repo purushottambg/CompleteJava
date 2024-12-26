dfea8df93f84d76fcbf093e83190c28e13864275
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-26 20:36:25 +0530

    Concurrency example

---
+++ threading/Concurrency.java
@@ -0,0 +1,18 @@
+package threading;
+
+public class Concurrency extends Thread{
+    private static Integer globalVariable=0;
+    public static void main(String[] args) {
+        Concurrency concurrency = new Concurrency();
+        //concurrency.start();
+        concurrency.run();
+        if(!concurrency.isAlive())
+            globalVariable=globalVariable+3;
+        System.out.println("In main Global Variable: "+globalVariable);
+
+    }
+    public void run(){
+        globalVariable++;
+        System.out.println("Value of the Global Variable: "+globalVariable);
+    }
+}
