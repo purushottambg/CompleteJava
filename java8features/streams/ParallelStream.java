9319d037daac545ed3549833bb7db8066d107e00
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-27 12:21:15 +0530

    more examples added

---
+++ java8features/streams/ParallelStream.java
@@ -0,0 +1,13 @@
+package java8features.streams;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
+
+public class ParallelStream {
+    public static void main(String[] args) {
+        Stream<Integer> parallelStream = Stream.generate(()->1*2).limit(250);
+        List<Integer> items = parallelStream.skip(0).collect(Collectors.toList());
+        System.out.println(items);
+    }
+}
