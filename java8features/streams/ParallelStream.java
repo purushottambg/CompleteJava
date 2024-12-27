1e73b8ec1f1805a4f844a0b15a8dc2dfe2d9596a
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-27 12:23:16 +0530

    Parallel Streams

--- java8features/streams/ParallelStream.java
+++ java8features/streams/ParallelStream.java
@@ -8,6 +8,7 @@ public class ParallelStream {
     public static void main(String[] args) {
         Stream<Integer> parallelStream = Stream.generate(()->1*2).limit(250);
         List<Integer> items = parallelStream.skip(0).collect(Collectors.toList());
+        items.parallelStream();
         System.out.println(items);
     }
 }
