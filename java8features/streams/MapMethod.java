6875cc55a6fed3948847ade21dac1fd96eb2a10a
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-27 11:40:37 +0530

    used operations like sorted, distinct and map also

--- java8features/streams/MapMethod.java
+++ java8features/streams/MapMethod.java
@@ -1,7 +1,14 @@
 package java8features.streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MapMethod {
     public static void main(String[] args) {
-
+        Stream<Integer> radStream = Stream.generate(()->(int) (Math.random()*100)).limit(50);
+        System.out.println("Total ist:\n"+ radStream.toString());
+        List<Integer> mappedList = radStream.filter(n->n%2!=0).map(n->n/3).sorted().distinct().collect(Collectors.toList());
+        System.out.println(mappedList);
     }
 }
