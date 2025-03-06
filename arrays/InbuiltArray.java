e70b7984ee231512ced10ff0aa1ee51a38db8641
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-03-06 23:24:00 +0530

    Added the inbuilt array and it's function

---
+++ arrays/InbuiltArray.java
@@ -0,0 +1,11 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class InbuiltArray {
+    public static void main(String[] args) {
+        int[] inbuiltArray = {332,5,765,234,54,234,65,34,6,34,657,23,5,63,12,78,97,67,645,3453,34,23,12,63,18};
+        System.out.println(Arrays.binarySearch(inbuiltArray, 23));;
+    }
+}
--- challenges/SortEmployees.java
+++ challenges/SortEmployees.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Employees{
     String name;
@@ -18,21 +19,37 @@ class Employees{
     }
 }
 
+//public class SortEmployees {
+//    public static void main(String[] args) {
+//
+//
+//        List<Employees> employeesList = Arrays.asList(
+//                new Employees("Purushottam", 23),
+//                new Employees("Purushottam", 25),
+//                new Employees("Raja", 23),
+//                new Employees("Vikas", 23),
+//                new Employees("Vikas", 3)
+//
+//        );
+//        employeesList.forEach(System.out::println);
+//        System.out.println("After Sorting: ");
+//        employeesList.sort(Comparator.comparing((Employees e) -> e.name).thenComparing( e -> e.salary));
+//        employeesList.forEach(System.out::println);
+//    }
+//}
+
 public class SortEmployees {
     public static void main(String[] args) {
-
-
-        List<Employees> employeesList = Arrays.asList(
+        List<Employees> emps = Arrays.asList(
                 new Employees("Purushottam", 23),
                 new Employees("Purushottam", 25),
                 new Employees("Raja", 23),
                 new Employees("Vikas", 23),
                 new Employees("Vikas", 3)
-
         );
-        employeesList.forEach(System.out::println);
-        System.out.println("After Sorting: ");
-        employeesList.sort(Comparator.comparing((Employees e) -> e.name).thenComparing( e -> e.salary));
-        employeesList.forEach(System.out::println);
+
+        List<Employees> sortedEmps = emps.stream().sorted(Comparator.comparing((Employees e) -> e.name).thenComparing((Employees e)->e.salary)).collect(Collectors.toList());
+        System.out.println("After comparison");
+        sortedEmps.forEach(System.out::println);
     }
 }
