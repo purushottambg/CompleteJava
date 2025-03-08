13fc165ee89e0f266cd72c539ad70a00b0961cc5
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-03-08 14:32:17 +0530

    Final keyword understood

---
+++ basic/FinalKeyword.java
@@ -0,0 +1,37 @@
+package basic;
+
+final class One{
+   static final int initialValue=0;
+
+   public void greet(String name){
+        System.out.println("Good to see you mr."+name);
+   }
+
+}
+class Two{
+    final int AgeLimit=0;
+    int countInst=0;
+    private int number;
+    private String name;
+    Two(){}
+    Two(int n, String na){
+        this.number=n;
+        this.name=na;
+        countInst++;
+    }
+    final void printData(){
+        System.out.println("Current count is: "+countInst);
+        System.out.println(toString()+" and ageLimit is: "+ AgeLimit);
+    }
+}
+public class FinalKeyword extends Two{
+
+    public static void main(String[] args) {
+        One one = new One();
+        one.greet("Purushottam");
+        Two two = new Two(32, "Purushottam");
+        if(two.AgeLimit<=0) System.out.println("Age limit is Zero");
+
+
+    }
+}
