132ad8d14293cc0d53705edf27243d34285e94da
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-03-11 03:27:02 +0530

    values are printing correctly

--- challenges/Fibonacci.java
+++ challenges/Fibonacci.java
@@ -1,18 +1,22 @@
 package challenges;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Fibonacci {
     public static void main(String[] args) {
         System.out.print("Enter number to print the series: ");
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        System.out.println(rec(num));
+        Long num = scanner.nextLong();
+        for(Long i : LongStream.range(0, num).toArray()){
+            System.out.print(rec(i)+", ");
+        }
     }
 
-    private static int rec(int num){
+    private static Long rec(Long num){
         if(num<=1){
-            return 1;
+            return num;
         } else {
              return rec(num-1)+rec(num-2);
         }
