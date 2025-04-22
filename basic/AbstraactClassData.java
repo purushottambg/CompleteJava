511a03e4fe87ac5e14832d9114c3f3fa932db0c0
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-22 23:27:34 +0530

    Abstract can have static data members whereas they are not static by default like interfaces

--- basic/AbstraactClassData.java
+++ basic/AbstraactClassData.java
@@ -1,11 +1,11 @@
 package basic;
 
-abstract class One{
-    Integer val=12;
+abstract class OneAbstract{
+    static  Integer val=12;
 }
 
 public class AbstraactClassData {
     public static void main(String[] args) {
-        System.out.println(""+ One.val);
+        System.out.println("Abstract class data member: "+ OneAbstract.val);
     }
 }
