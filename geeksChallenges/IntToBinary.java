4f8de01159f784b50bb617aae60ab94cb84b3bcc
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-23 23:35:43 +0530

    Binary one is added

---
+++ geeksChallenges/IntToBinary.java
@@ -0,0 +1,13 @@
+package geeksChallenges;
+
+import java.util.Scanner;
+
+public class IntToBinary {
+    public static void main(String[] args) {
+        System.out.print("Enter number to convert to the binary: ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+
+    }
+}
