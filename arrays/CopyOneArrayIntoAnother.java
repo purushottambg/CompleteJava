826b699afea4aaf0d2f159cc02879233c0064f90
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-10 15:08:51 +0530

    array copy done

--- arrays/CopyOneArrayIntoAnother.java
+++ arrays/CopyOneArrayIntoAnother.java
@@ -1,7 +1,16 @@
 package arrays;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class CopyOneArrayIntoAnother {
     public static void main(String[] args) {
+        Integer[] arrayOne =  {12,32,3,46,76,45,34,23,4,67,34,35,34};
+        Integer[] arrayTwo = new Integer[arrayOne.length];
+        arrayTwo = arrayOne;
 
+        for (Integer i: arrayTwo){
+            System.out.println(i);
+        }
     }
 }
