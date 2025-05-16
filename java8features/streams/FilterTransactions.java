effd6618ac6f984755458b2c156df12bed09c362
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-16 23:00:55 +0530

    created stream which can filter the fraudlant transcations from the nested list

---
+++ java8features/streams/FilterTransactions.java
@@ -0,0 +1,10 @@
+package java8features.streams;
+
+/**
+ * filter the given transaction which are suspicious like having multiple locations and threshold above 10000
+ */
+public class FilterTransactions {
+    public static void main(String[] args) {
+
+    }
+}
