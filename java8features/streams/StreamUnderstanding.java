578cc16ecc9f084840c915c6e87b6ccbe900ff8c
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-17 10:25:18 +0530

    Small method of stream like generate, iterate, of are used

---
+++ java8features/streams/StreamUnderstanding.java
@@ -0,0 +1,8 @@
+package java8features.streams;
+
+/**
+ * Collection of variables from the collection which can perform finctional anddeclarative method operation
+ * Advantages of stream are readability, flexibility, parallelism, encapsilation
+ */
+public class StreamUnderstanding {
+}
