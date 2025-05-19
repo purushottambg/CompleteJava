4f80125d7d9cb752c7fbc446699db50c7e496305
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-19 12:25:19 +0530

    Adding the solution for the sorting stream

--- challenges/HighestOfEachCity.java
+++ challenges/HighestOfEachCity.java
@@ -1,7 +1,8 @@
 package challenges;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 class Merchants{
@@ -47,25 +48,60 @@ class Merchants{
     public void setIncome(double income) {
         this.income = income;
     }
+
+    @Override
+    public String toString() {
+        return "Details are id: " + id+ " name: "+
+                name+ " city: "+
+                city+ " income: "+
+                income+ " ";
+    }
 }
 
 public class HighestOfEachCity {
     public static void main(String[] args) {
         List<Merchants> shopkeepers = Arrays.asList(
                 new Merchants(1L, "Amit Traders", "Mumbai", 95000),
-                new Merchants(2L, "Sharma Mart", "Delhi", 87000),
+                new Merchants(2L, "Sharma Mart", "Pune", 87000),
                 new Merchants(3L, "Verma Stores", "Bangalore", 76000),
-                new Merchants(4L, "Kapoor Bazaar", "Hyderabad", 99000),
+                new Merchants(4L, "Kapoor Bazaar", "Pune", 99000),
                 new Merchants(5L, "Joshi & Sons", "Chennai", 91000),
                 new Merchants(6L, "Gupta Goods", "Pune", 84000),
                 new Merchants(7L, "Reddy Retail", "Kolkata", 88000),
-                new Merchants(8L, "Patel Traders", "Ahmedabad", 96000),
+                new Merchants(8L, "Patel Traders", "Pune", 96000),
                 new Merchants(9L, "Yadav Wholesale", "Jaipur", 72000),
-                new Merchants(10L, "Thakur Exports", "Lucknow", 93000)
+                new Merchants(10L, "Thakur Exports", "Jaipur", 93000)
         );
 
         List<Merchants> highestEarnerFromEachCity = shopkeepers.stream()
-                .sorted()
+                .sorted(Comparator.comparing(Merchants::getCity).thenComparing(Comparator.comparingDouble(Merchants::getIncome).reversed()))
+                .filter(new Predicate<Merchants>() {
+                    Set<String> uniqueCities = new HashSet<>();
+                    @Override
+                    public boolean test(Merchants merchants) {
+                        return uniqueCities.add(merchants.getCity());  //only one merchant will be returned here as we've already sorted them by their income
+                    }
+                })
                 .collect(Collectors.toList());
+
+        System.out.println(highestEarnerFromEachCity);
+
+        for (Merchants merchants: highestEarnerFromEachCity){
+            System.out.println(merchants.toString());
+        }
+
+        System.out.println("With one line solution: ");
+
+//        Map<String, Merchants> topEarners = shopkeepers.stream()
+//                .collect(Collectors.toMap(
+//                        Merchants::getCity,                            // key = city
+//                        Function.identity()                        // value = Merchant object
+//                        //BinaryOperator.maxBy(Comparator.comparingDouble(Merchants::getIncome)) // resolve duplicate cities
+//                ));
+//
+//        shopkeepers.forEach((city, merchant) ->
+//                System.out.println("City: " + city + ", Top Earner: " + merchant.getName() + ", Income: " + merchant.getIncome())
+//        );
+
     }
 }
