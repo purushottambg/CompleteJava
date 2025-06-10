a208d7c09f9b045f67c8833ddeb75f12666b77b0
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-06-10 11:32:20 +0530

    Map Predicate

---
+++ java8features/predicate/PredicateAllMethods.java
@@ -0,0 +1,23 @@
+package java8features.predicate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class PredicateAllMethods {
+    public static void main(String[] args) {
+        System.out.println("Here we'll see the predicates and their usage");
+        Map<Integer, String> rollNos = new HashMap<>();
+        rollNos.put(1, "Om");
+        rollNos.put(2, "Namo");
+        rollNos.put(3, "Shivai");
+
+        Predicate<Map> rollNo = x->rollNos.containsKey(2);
+
+        System.out.println("does Shivai has larger than 2 number:"+rollNo.test(rollNos));
+
+
+
+    }
+}
