2ad960a747b8dd8f87603d134244ca01882949a8
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-06-11 22:22:31 +0530

    Added the consumer class

---
+++ java8features/consumer/ConsumeInt.java
@@ -0,0 +1,21 @@
+package java8features.consumer;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+/**
+ * The Consumer is a functional interface with
+ */
+public class ConsumeInt {
+    public static void main(String[] args) {
+        Consumer<Integer> printNext = x -> System.out.println(x+1);
+
+        Integer[] nums = IntStream.range(123, 128)
+                    .boxed()
+                        .toArray(Integer[]::new);
+        for (Integer i: nums){
+            printNext.accept(i);
+        }
+    }
+}
