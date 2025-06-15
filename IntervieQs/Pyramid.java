1985c67c6bf8a0b86ec9ab3d63c5213bd9a2b63f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-06-15 15:14:36 +0530

    Added the pyramid updated program

--- IntervieQs/Pyramid.java
+++ IntervieQs/Pyramid.java
@@ -5,14 +5,19 @@ public class Pyramid {
         printPyramid(7);
     }
 
-    public static void printPyramid(int num){
-        for(int i=1;i<=num;i++){
-            for(int j=0;j<=num;j++){
-
-                for(int k=j/2; k<j;k++){
-                    System.out.print(" * ");
-                }
+    public static void printPyramid(int n){
+        for (int i=0; i<n; i++)
+        {
+            for (int j=n-i; j>1; j--)
+            {
+                System.out.print(" ");
             }
+
+            for (int j=0; j<=i; j++ )
+            {
+                System.out.print("* ");
+            }
+
             System.out.println();
         }
     }
