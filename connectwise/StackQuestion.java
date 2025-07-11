1228cb707f9b2cae8d1183b1a091592884019d05
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-07-11 23:44:14 +0530

    Added the queue implementation with the stack

--- connectwise/StackQuestion.java
+++ connectwise/StackQuestion.java
@@ -1,23 +1,37 @@
 package connectwise;
 
 import java.util.Scanner;
+import java.util.Stack;
+
+class MyStack {
+    Stack<Integer> stack = new Stack<>();
+    public MyStack() {
+
+    }
+
+    public void push(int x) {
+        stack.push(x);
+    }
+
+    public int pop() {
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
 
 public class StackQuestion {
     public static void main(String[] args) {
-        System.out.println("Enter the number:");
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int squareRoot=0;
-        for (int i = 1; i < num/2; i++) {
-            for (int j = 1; j < num/2; j++) {
-                if (i*j==num && i==j){
-                    squareRoot=i;
-                    System.out.println(i+" is the square root");
-                    break;
-                }
-            }
-        }
-        if (squareRoot==0)
-            System.out.println("There's no full square root for "+num);
+        MyStack obj = new MyStack();
+        obj.push(12);
+        int param_2 = obj.pop();
+        int param_3 = obj.top();
+        boolean param_4 = obj.empty();
     }
 }
